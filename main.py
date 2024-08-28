c63b0a753c40396b3987371cac551d80bb554cb8
PopanCristian <p_cristi10@yahoo.com>
2024-08-28 19:24:32 +0300

    Design for butons

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ class Clicker:
         self.app = app
         self.app.title("MyAutoClicker")
         self.frame = tk.Frame(app)
-        self.frame.pack(pady=120, padx=120) #dimensions of frame
+        self.frame.pack(pady=50, padx=80) #dimensions of frame
 
 
-        self.delay_entry = tk.Entry(self.frame, width=15, fg='grey', justify='center')
+        self.delay_entry = tk.Entry(self.frame, bg="#cab287", width=15, fg='grey', justify='center')
         self.delay_entry.insert(0, "Delay")
         self.delay_entry.bind('<FocusIn>', self.dataIN)
-        self.delay_entry.pack()
+        self.delay_entry.pack(pady=10)
 
-        self.button_to_start = tk.Button(self.frame, width=12, text="Start")
-        self.button_to_start.pack()
+        self.button_to_start = tk.Button(self.frame, bg="#cab287", fg="red", width=12, text="Start")
+        self.button_to_start.pack(pady=10)
 
-        self.button_to_stop = tk.Button(self.frame, width=12, text="Stop", command=app.destroy)
-        self.button_to_stop.pack()
+        self.button_to_stop = tk.Button(self.frame, bg="#cab287", fg="#404040", width=12, text="Stop", command=app.destroy)
+        self.button_to_stop.pack(pady=10)
 
     def dataIN(self, event): #I need to delete the informational text and put the timer
         if self.delay_entry.get() == "Delay":
@@ -29,6 +29,7 @@ class Clicker:
             self.delay = float(self.delay_entry.get())
         except:
             self.delay = 1.0
+
 
 
 if __name__ == "__main__":
